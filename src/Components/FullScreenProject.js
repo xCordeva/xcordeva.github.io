ff4a912b06518123e9fab4705c417397ff3e7f0e
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-07-04 04:31:32 +0300

    feat: remove "View Code" for projects with private repos

--- src/Components/FullScreenProject.js
+++ src/Components/FullScreenProject.js
@@ -1,12 +1,11 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { closeFullScreen } from '../Features/ProjectFullScreen';
-import { projectsData } from '../Data/ProjectsData';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { faTowerBroadcast } from '@fortawesome/free-solid-svg-icons'
+import { useSelector, useDispatch } from "react-redux";
+import { closeFullScreen } from "../Features/ProjectFullScreen";
+import { projectsData } from "../Data/ProjectsData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faTowerBroadcast } from "@fortawesome/free-solid-svg-icons";
 
 export default function FullScreenProject() {
-
   /* getting the selected project using redux state */
   const selectedProject = useSelector((state) => state.ProjectFullScreen.value);
   const dispatch = useDispatch();
@@ -14,16 +13,17 @@ export default function FullScreenProject() {
   if (selectedProject === null) {
     return null;
   }
-  
+
   /* getting the clicked project */
   const project = projectsData[selectedProject];
 
   return (
     <div className="full-screen" tabIndex={-1}>
-
       <div className="fullscreen-project">
-
-        <span className="close-icon" onClick={() => dispatch(closeFullScreen())}>
+        <span
+          className="close-icon"
+          onClick={() => dispatch(closeFullScreen())}
+        >
           &#10006;
         </span>
         <img src={project.image} alt="" />
@@ -35,22 +35,32 @@ export default function FullScreenProject() {
         </div>
 
         <h2>{project.title}</h2>
-        <h5 className='project-date'>{project.date}</h5>
+        <h5 className="project-date">{project.date}</h5>
         <p>{project.details}</p>
 
         <div className="project-links">
-          <a href={`${project.githubLink}`} target='_blank' className="link-project">
-          <FontAwesomeIcon icon={faGithub}/>
-            View Code
-          </a>
-          <a href={`${project.liveLink}`} target='_blank' className="link-project">
+          {project.githubLink && (
+            <a
+              href={`${project.githubLink}`}
+              target="_blank"
+              rel="noreferrer"
+              className="link-project"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+              View Code
+            </a>
+          )}
+          <a
+            href={`${project.liveLink}`}
+            target="_blank"
+            rel="noreferrer"
+            className="link-project"
+          >
             <FontAwesomeIcon icon={faTowerBroadcast} />
-            View Live App
+            View Live
           </a>
         </div>
-
       </div>
-
     </div>
   );
-};
+}
